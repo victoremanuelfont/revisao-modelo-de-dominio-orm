cd92f72b544898932181f06c0f96e724346498b0
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-10-30 15:58:21 -0300

    Mapeamento Relacionamento um-para-muitos

--- src/main/java/com/revisaocapdois/dscommerce/entities/User.java
+++ src/main/java/com/revisaocapdois/dscommerce/entities/User.java
@@ -3,6 +3,8 @@ package com.revisaocapdois.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_user")
@@ -16,6 +18,9 @@ public class User {
     private String phone;
     private LocalDate birthDate;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User(){
     }
