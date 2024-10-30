77cf473c499b21cb920db42894139d8487d8e2f8
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-10-30 16:26:48 -0300

    Anotations para a classe Payment

--- src/main/java/com/revisaocapdois/dscommerce/entities/Payment.java
+++ src/main/java/com/revisaocapdois/dscommerce/entities/Payment.java
@@ -1,10 +1,17 @@
 package com.revisaocapdois.dscommerce.entities;
 
+import jakarta.persistence.*;
+
 import java.time.Instant;
 
+@Entity
+@Table(name = "tb_payment")
 public class Payment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
 
     public Payment(Long id, Instant moment) {
