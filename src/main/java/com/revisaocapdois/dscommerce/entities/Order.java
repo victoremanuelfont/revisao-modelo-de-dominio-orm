29950d98bc933bce1193219ccf8e046e54f46bf8
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-10-30 16:16:43 -0300

    Construtores e getter e setter da classe Order

--- src/main/java/com/revisaocapdois/dscommerce/entities/Order.java
+++ src/main/java/com/revisaocapdois/dscommerce/entities/Order.java
@@ -11,7 +11,7 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
      private Long id;
-    
+
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
      private Instant moment;
      private OrderStatus status;
@@ -20,4 +20,45 @@ public class Order {
      @JoinColumn(name = "client_id")
      private User client;
 
+    public Order() {
+    }
+
+    public Order(Instant moment, Long id, OrderStatus status, User client) {
+        this.moment = moment;
+        this.id = id;
+        this.status = status;
+        this.client = client;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
 }
