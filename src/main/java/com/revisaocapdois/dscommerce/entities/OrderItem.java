2c2eeb45582a0a84d33b70276d6599afa2429267
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-10-31 16:35:50 -0300

    Anotations na classe OrderItem

--- src/main/java/com/revisaocapdois/dscommerce/entities/OrderItem.java
+++ src/main/java/com/revisaocapdois/dscommerce/entities/OrderItem.java
@@ -1,7 +1,15 @@
 package com.revisaocapdois.dscommerce.entities;
 
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order_item")
 public class OrderItem {
 
+    @EmbeddedId
     private OrderItemPK id = new OrderItemPK();
     private Integer quantity;
     private Double price;
