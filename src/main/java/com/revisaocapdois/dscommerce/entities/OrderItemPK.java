23c3f9132f6757f01a71161cc4abfc8c0e729366
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-10-31 16:19:02 -0300

    Classe a parte que representa a chave primaria composta de OrderItem

---
+++ src/main/java/com/revisaocapdois/dscommerce/entities/OrderItemPK.java
@@ -0,0 +1,20 @@
+package com.revisaocapdois.dscommerce.entities;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Embeddable
+public class OrderItemPK {
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+
+
+
+}
